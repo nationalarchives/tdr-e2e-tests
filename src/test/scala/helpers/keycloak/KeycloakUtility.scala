00c15048e79b4cd9f7b79d66691aeca0a8b64b23
TomJKing <tom.king@nationalarchives.gov.uk>
2020-08-27 14:13:49 +0100

    Abstract bearer access token retrieval
    Both GraphQLClient and KeycloakClient need to retrieve bearer access tokens to perform actions
    
    Abstracted the existing bearer access token retrieve function from the GraphQLClient to separate object to allow re-use

---
+++ src/test/scala/helpers/keycloak/KeycloakUtility.scala
@@ -0,0 +1,34 @@
+package helpers.keycloak
+import com.nimbusds.oauth2.sdk.token.BearerAccessToken
+import com.typesafe.config.ConfigFactory
+import io.circe.generic.auto._
+import sttp.client.circe.asJson
+import sttp.client.{HttpURLConnectionBackend, Identity, NothingT, SttpBackend, basicRequest, _}
+
+import scala.language.postfixOps
+
+object KeycloakUtility {
+  val configuration = ConfigFactory.load
+
+  def bearerAccessToken(requestBody: Map[String, String]) = {
+
+    implicit val backend: SttpBackend[Identity, Nothing, NothingT] = HttpURLConnectionBackend()
+
+    val authUrl = configuration.getString("tdr.auth.url")
+
+    val response = basicRequest
+      .body(requestBody)
+      .post(uri"$authUrl/auth/realms/tdr/protocol/openid-connect/token")
+      .response(asJson[AuthResponse])
+      .send()
+
+    val authResponse = response.body match {
+      case Right(body) =>
+        body
+      case Left(e) => throw e
+    }
+    new BearerAccessToken(authResponse.access_token)
+  }
+}
+
+case class AuthResponse(access_token: String)

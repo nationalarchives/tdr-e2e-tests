8d48663e071eba78c38de22dfe1423c13b45df17
TomJKing <tom.king@nationalarchives.gov.uk>
2020-08-27 14:59:06 +0100

    Refactor GraphqlClient to use utility for token retrieval

--- src/test/scala/helpers/graphql/GraphqlClient.scala
+++ src/test/scala/helpers/graphql/GraphqlClient.scala
@@ -3,11 +3,8 @@ package helpers.graphql
 import com.nimbusds.oauth2.sdk.token.BearerAccessToken
 import com.typesafe.config.ConfigFactory
 import helpers.keycloak.{KeycloakUtility, UserCredentials}
-import io.circe.generic.auto._
 import io.circe.{Decoder, Encoder}
 import sangria.ast.Document
-import sttp.client.circe.asJson
-import sttp.client.{HttpURLConnectionBackend, Identity, NothingT, SttpBackend, basicRequest, _}
 import uk.gov.nationalarchives.tdr.GraphQLClient
 
 import scala.concurrent.Await
@@ -17,32 +14,15 @@ import scala.language.postfixOps
 
 class GraphqlClient[Data, Variables](userCredentials: UserCredentials)(implicit val decoder: Decoder[Data], val encoder: Encoder[Variables]) {
   val configuration = ConfigFactory.load
+  val body: Map[String, String] = Map(
+    "grant_type" -> "password",
+    "username" -> userCredentials.userName,
+    "password" -> userCredentials.password,
+    "client_id" -> "tdr-fe"
+  )
 
   def userToken: BearerAccessToken = {
-    case class AuthResponse(access_token: String)
-    implicit val backend: SttpBackend[Identity, Nothing, NothingT] = HttpURLConnectionBackend()
-
-    val body: Map[String, String] = Map(
-      "grant_type" -> "password",
-      "username" -> userCredentials.userName,
-      "password" -> userCredentials.password,
-      "client_id" -> "tdr-fe"
-    )
-
-    val authUrl = configuration.getString("tdr.auth.url")
-
-    val response = basicRequest
-      .body(body)
-      .post(uri"$authUrl/auth/realms/tdr/protocol/openid-connect/token")
-      .response(asJson[AuthResponse])
-      .send()
-
-    val authResponse = response.body match {
-      case Right(body) =>
-        body
-      case Left(e) => throw e
-    }
-    new BearerAccessToken(authResponse.access_token)
+    KeycloakUtility.bearerAccessToken(body)
   }
 
   def result(document: Document, variables: Variables) = {

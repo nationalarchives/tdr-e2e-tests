7b10f2ddc234dc63ef1f5f87538dc0cc53c4d822
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-06-01 13:11:49 +0100

    Use python string formatting

--- .github/scripts/get_driver_location.py
+++ .github/scripts/get_driver_location.py
@@ -2,8 +2,8 @@ import sys
 environment = sys.argv[2]
 if len(sys.argv) == 1 or sys.argv[1] == "Firefox":
     print(f"::set-output name=driver::/opt/geckodriver")
-    print(f"::set-output name=lambda-name::tdr-e2e-tests-${environment}")
+    print(f"::set-output name=lambda-name::tdr-e2e-tests-{environment}")
 else:
     print(f"::set-output name=driver::/opt/chromedriver/chromedriver")
-    print(f"::set-output name=lambda-name::tdr-e2e-tests-chrome-${environment}")
+    print(f"::set-output name=lambda-name::tdr-e2e-tests-chrome-{environment}")
 

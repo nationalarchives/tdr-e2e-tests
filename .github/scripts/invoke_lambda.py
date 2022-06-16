f7809cfeb9c17a41ecd87916dce5a17e6b961174
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-06-16 18:50:11 +0100

    Set the connect timeout

--- .github/scripts/invoke_lambda.py
+++ .github/scripts/invoke_lambda.py
@@ -1,14 +1,16 @@
 import boto3
+from botocore.client import  Config
 import sys
 import json
 
-client = boto3.client("lambda")
+config = Config(connect_timeout=600, read_timeout=600)
+client = boto3.client("lambda", config=config)
 environment = sys.argv[1]
 file = sys.argv[2]
 url = sys.argv[3]
 browser = sys.argv[4]
 
-payload = {"feature": file, "nodeUrl": f"http://{url}:4444", "browser": browser}
-response = client.invoke(FunctionName=f"tdr-e2e-tests-{environment}", Payload=bytes(json.dumps(payload), "utf-8"))
+payload = bytes(json.dumps({"feature": file, "nodeUrl": f"http://{url}:4444", "browser": browser}), "utf-8")
+response = client.invoke(FunctionName=f"tdr-e2e-tests-{environment}", Payload=payload)
 if 'FunctionError' in response and response['FunctionError'] is not None:
     exit(1)

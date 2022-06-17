dd908387bcf2c557f2ab3b92419ee993850e914f
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-06-17 13:51:18 +0100

    Use the consignmentapi cluster

--- .github/scripts/stop_ecs_task.py
+++ .github/scripts/stop_ecs_task.py
@@ -4,5 +4,5 @@ import sys
 stage = sys.argv[1]
 task_id = sys.argv[2]
 client = boto3.client('ecs')
-cluster = f"file_format_build_{stage}"
+cluster = f"consignmentapi_{stage}"
 response = client.stop_task(cluster=cluster, task=task_id)

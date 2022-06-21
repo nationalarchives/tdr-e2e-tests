28151a6d566b0adf5917339960a1199aab9c4b53
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-06-21 13:09:41 +0100

    Don't run ECS task

--- .github/scripts/invoke_lambda.py
+++ .github/scripts/invoke_lambda.py
@@ -7,8 +7,7 @@ config = Config(connect_timeout=600, read_timeout=600)
 client = boto3.client("lambda", config=config)
 environment = sys.argv[1]
 file = sys.argv[2]
-url = sys.argv[3]
-browser = sys.argv[4]
+browser = sys.argv[3]
 
 payload = bytes(json.dumps({"feature": file, "browser": browser}), "utf-8")
 response = client.invoke(FunctionName=f"tdr-e2e-tests-{environment}", Payload=payload)

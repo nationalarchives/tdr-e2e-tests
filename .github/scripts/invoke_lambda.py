cea6570f49b8a12985401d13462be29fe4c6793d
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-06-22 14:13:17 +0100

    Try to debug long running task

--- .github/scripts/invoke_lambda.py
+++ .github/scripts/invoke_lambda.py
@@ -2,6 +2,9 @@ import boto3
 from botocore.client import  Config
 import sys
 import json
+from time import time
+
+start = time()
 
 config = Config(connect_timeout=600, read_timeout=600)
 client = boto3.client("lambda", config=config)
@@ -10,6 +13,9 @@ file = sys.argv[2]
 browser = sys.argv[3]
 
 payload = bytes(json.dumps({"feature": file, "browser": browser}), "utf-8")
+print("Calling lambda")
+print(start)
 response = client.invoke(FunctionName=f"tdr-e2e-tests-{environment}", Payload=payload)
 if 'FunctionError' in response and response['FunctionError'] is not None:
     exit(1)
+print(time() - start)

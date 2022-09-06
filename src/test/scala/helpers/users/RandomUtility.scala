e2bf7cab34a34dfc8214017a61e6ba3a87cd07f1
thanhz <thanhz@hotmail.com>
2022-09-06 17:45:28 +0100

    Modify regex to meet current password policy

--- src/test/scala/helpers/users/RandomUtility.scala
+++ src/test/scala/helpers/users/RandomUtility.scala
@@ -3,12 +3,10 @@ package helpers.users
 import org.apache.commons.lang3.RandomStringUtils
 
 import java.util.regex.Pattern
-import scala.util.Random
 
 object RandomUtility {
 
   def randomString(length: Int = 8): String = {
-//    Random.alphanumeric.dropWhile(_.isDigit).take(length).mkString
     var randomString = ""
     do {
       randomString = RandomStringUtils.randomAlphanumeric(length)
@@ -18,11 +16,8 @@ object RandomUtility {
   }
 
   def checkPassword(s: String): Boolean = {
-    /*Check password contains:
-    - at least 8 characters
-    - must contain at least 1 uppercase letter, 1 lowercase letter, and 1 number
-    - Can contain special characters*/
-    val regex = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$"
+    /*Must be at least 8 characters, must contain at least 1 uppercase letter and 1 number*/
+    val regex = "^(?=.*\\d)(?=.*[A-Z])(?=.*[A-Z]).{8,}$"
     val p = Pattern.compile(regex)
     p.matcher(s).matches()
   }

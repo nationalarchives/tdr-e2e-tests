b30c398bdf57c333e1deca982834828ae9174a96
thanhz <thanhz@hotmail.com>
2022-09-06 16:23:39 +0100

    utilise randomAlphanumeric from RandomStringUtils

--- src/test/scala/helpers/users/RandomUtility.scala
+++ src/test/scala/helpers/users/RandomUtility.scala
@@ -1,10 +1,13 @@
 package helpers.users
 
+import org.apache.commons.lang3.RandomStringUtils
+
 import scala.util.Random
 
 object RandomUtility {
 
   def randomString(length: Int = 8): String = {
-    Random.alphanumeric.dropWhile(_.isDigit).take(length).mkString
+//    Random.alphanumeric.dropWhile(_.isDigit).take(length).mkString
+    RandomStringUtils.randomAlphanumeric(length)
   }
 }

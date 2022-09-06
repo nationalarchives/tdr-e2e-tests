6ad4418eb7383ef8194df805406867df466a3f79
thanhz <thanhz@hotmail.com>
2022-09-07 00:19:36 +0100

    regex remove repeated check for uppercase

--- src/test/scala/helpers/users/RandomUtility.scala
+++ src/test/scala/helpers/users/RandomUtility.scala
@@ -19,7 +19,7 @@ object RandomUtility {
 
   def checkPassword(s: String): Boolean = {
     /*Must be at least 8 characters, must contain at least 1 uppercase letter and 1 number*/
-    val regex = "^(?=.*\\d)(?=.*[A-Z])(?=.*[A-Z]).{8,}$"
+    val regex = "^(?=.*\\d)(?=.*[A-Z]).{8,}$"
     val p = Pattern.compile(regex)
     p.matcher(s).matches()
   }

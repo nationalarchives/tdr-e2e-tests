f20d2e7b2cb3fb4b26ba1e60f4635bab96743d40
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2023-03-01 16:20:45 +0000

    Read the instructions

--- .github/scripts/get_keycloak_secrets.py
+++ .github/scripts/get_keycloak_secrets.py
@@ -18,8 +18,8 @@ def get_client_secret(client_secret_path):
 user_admin_secret = get_client_secret(f"/{env}/keycloak/user_admin_client/secret")
 backend_checks_secret = get_client_secret(f"/{env}/keycloak/backend_checks_client/secret")
 with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
-    print(f"=user_admin_secret={user_admin_secret}", file=fh)
-    print(f"=backend_checks_secret={backend_checks_secret}", file=fh)
+    print(f"user_admin_secret={user_admin_secret}", file=fh)
+    print(f"backend_checks_secret={backend_checks_secret}", file=fh)
 
 print(f"::add-mask::{backend_checks_secret}")
 print(f"::add-mask::{user_admin_secret}")

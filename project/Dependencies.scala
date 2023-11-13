50e6d34137e92f6ac957b1f1ce396b570385e7e3
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2023-11-13 08:27:14 +0000

    Update s3, sts to 2.21.21

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  private val awsSdkVersion = "2.21.20"
+  private val awsSdkVersion = "2.21.21"
   private val circeVersion = "0.14.6"
   private val cucumberCoreVersion = "7.14.0"
   private val cucumberScalaVersion = "8.18.0"

9c4c3ccd78bc2b4eb8e38833c794d305f3c2b0bd
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2023-11-28 08:28:00 +0000

    Update cucumber-core, cucumber-junit to 7.14.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,9 +3,9 @@ import sbt._
 object Dependencies {
   private val awsSdkVersion = "2.21.29"
   private val circeVersion = "0.14.6"
-  private val cucumberCoreVersion = "7.14.0"
+  private val cucumberCoreVersion = "7.14.1"
   private val cucumberScalaVersion = "8.18.0"
-  private val cucumberJUnitVersion = "7.14.0"
+  private val cucumberJUnitVersion = "7.14.1"
   private val keycloakVersion = "23.0.0"
   private val softwareMillVersion = "2.3.0"
 

baf3e39e5dafa2e218cf4211168dd276fbabec33
TomJKing <tom.king@nationalarchives.gov.uk>
2023-12-11 09:16:28 +0000

    Switch to '/json' ipinfo endpoint
    '/ip' endpoint is no longer available

--- .github/scripts/update-security-group.sh
+++ .github/scripts/update-security-group.sh
@@ -1,5 +1,5 @@
 GROUP_ID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=frontend-load-balancer-security-group | jq -r '.SecurityGroups[0].GroupId')
-IP=$(curl https://ipinfo.io/ip)
+IP=$(curl https://ipinfo.io/json | jq -r '.ip')
 if [ $1 = "INSERT" ]
 then
   aws ec2 authorize-security-group-ingress --group-id $GROUP_ID --protocol tcp --port 443 --cidr $IP/32

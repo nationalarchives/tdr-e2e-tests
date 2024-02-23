d96565b869dd6f74b319a275a9ec3ecf1c5f4e30
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2024-02-23 12:38:51 +0000

    Update keycloak-admin-client, ... to 23.0.7 in scala-steward-dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   private val cucumberCoreVersion = "7.15.0"
   private val cucumberScalaVersion = "8.20.0"
   private val cucumberJUnitVersion = "7.15.0"
-  private val keycloakVersion = "23.0.6"
+  private val keycloakVersion = "23.0.7"
   private val softwareMillVersion = "2.3.0"
 
   lazy val tdrGraphQlClient    = "uk.gov.nationalarchives" %% "tdr-graphql-client" % "0.0.146"

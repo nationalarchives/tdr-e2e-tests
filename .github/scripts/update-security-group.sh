40e6f8d0876c5b7dc1ce813dda8afd1118576e1c
VimleshGupta <vimlesh.gupta@nationalarchives.gov.uk>
2024-05-03 13:32:30 +0100

    Update update-security-group.sh

--- .github/scripts/update-security-group.sh
+++ .github/scripts/update-security-group.sh
@@ -6,20 +6,15 @@
 
 GROUP_ID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=frontend-load-balancer-security-group | jq -r '.SecurityGroups[0].GroupId')
 
-check_ip_authorized() {
-    local ip=$1
-    aws ec2 describe-security-groups --group-id "$GROUP_ID" \
-        --query "SecurityGroups[*].IpPermissions[*].IpRanges[?CidrIp=='$ip'].CidrIp" --output text
-}
+security_group_info=$(aws ec2 describe-security-groups --group-ids "$GROUP_ID")
 
 if [ "$1" = "INSERT" ]; then
   CURRENT_IP=$(curl -s https://ipinfo.io/ip)/32
-  CURRENT_IP_AUTHORISATION_RESPONSE=$(check_ip_authorized "$CURRENT_IP")
-  if [ -z "$CURRENT_IP_AUTHORISATION_RESPONSE" ]; then
+  if echo "$security_group_info" | grep -q "$CURRENT_IP"; then
+    echo "IP $CURRENT_IP is already authorized."
+  else
     aws ec2 authorize-security-group-ingress --group-id "$GROUP_ID" --protocol tcp --port 443 --cidr "$CURRENT_IP"
     echo "IP $CURRENT_IP has been authorized."
-  else
-    echo "IP $CURRENT_IP is already authorized."
   fi
 elif [ "$1" = "DELETE" ]; then
   if [ -z "$2" ]; then
@@ -27,9 +22,7 @@ elif [ "$1" = "DELETE" ]; then
     exit 1
   fi
   SUPPLIED_IP="$2/32"
-  SUPPLIED_IP_AUTHORISATION_RESPONSE=$(check_ip_authorized "$SUPPLIED_IP")
-  echo ">>>>>>>>>>>>$SUPPLIED_IP_AUTHORISATION_RESPONSE"
-  if [ -n "$SUPPLIED_IP_AUTHORISATION_RESPONSE" ]; then
+  if echo "$security_group_info" | grep -q "$SUPPLIED_IP"; then
     aws ec2 revoke-security-group-ingress --group-id "$GROUP_ID" --protocol tcp --port 443 --cidr "$SUPPLIED_IP"
     echo "IP $SUPPLIED_IP has been revoked."
   else

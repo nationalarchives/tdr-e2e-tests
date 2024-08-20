d175b3f8bae52279b36d65928efc441819f1a221
Thanh <thanh.lam@nationalarchives.gov.uk>
2024-08-20 18:12:53 +0100

    print page source for debug

--- src/test/scala/helpers/steps/StepsUtility.scala
+++ src/test/scala/helpers/steps/StepsUtility.scala
@@ -24,6 +24,7 @@ object StepsUtility {
       so it doesn't matter if the same element on the first page has disappeared.*/
       .until((driver: WebDriver) => {
         val panels: List[WebElement] = webDriver.findElements(By.className(elementClassName)).asScala.toList
+        println(${webDriver.getPageSource})
         panels.exists(_.getText == title)
       })
   }

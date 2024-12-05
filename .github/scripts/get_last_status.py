a3a6e923d5a85d1337e6aadd7683da9f2849b3a0
Thanh Lam <thanh.lam@nationalarchives.gov.uk>
2024-12-05 11:41:03 +0000

    Use workflow_pat in get_last_status.py

--- .github/scripts/get_last_status.py
+++ .github/scripts/get_last_status.py
@@ -1,6 +1,9 @@
 import requests
 import os
-resp = requests.get("https://api.github.com/repos/nationalarchives/tdr-e2e-tests/actions/workflows/ci.yml/runs").json()
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+url = "https://api.github.com/repos/nationalarchives/tdr-e2e-tests/actions/workflows/ci.yml/runs"
+headers = {"Authorization": f"Bearer {GITHUB_TOKEN}"}
+resp = requests.get(url, headers=headers).json()
 last_status = [x for x in resp["workflow_runs"] if x["event"] == "workflow_dispatch" and x["status"] == "completed"][0]["conclusion"]
 with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
     print(f"last_status={last_status}", file=fh)

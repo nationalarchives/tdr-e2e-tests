9d605c22346fe92bd3010a9320ec4f0e729db8a2
Thanh Lam <thanh.lam@nationalarchives.gov.uk>
2024-12-05 12:14:40 +0000

    print some information

--- .github/scripts/get_last_status.py
+++ .github/scripts/get_last_status.py
@@ -1,9 +1,10 @@
 import requests
 import os
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+print(if GITHUB_TOKEN == "":)
 url = "https://api.github.com/repos/nationalarchives/tdr-e2e-tests/actions/workflows/ci.yml/runs"
 headers = {"Authorization": f"Bearer {GITHUB_TOKEN}"}
-resp = requests.get(url, headers=headers)
+resp = requests.get(url, header=headers)
 print(f"Status Code: {resp.status_code}")
 print(f"Response: {resp.text}")
 last_status = [x for x in resp["workflow_runs"] if x["event"] == "workflow_dispatch" and x["status"] == "completed"][0]["conclusion"]

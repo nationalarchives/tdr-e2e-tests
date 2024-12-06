900b5c67b25c6e609053461ebd74bbc889865838
Thanh Lam <thanh.lam@nationalarchives.gov.uk>
2024-12-06 11:35:36 +0000

    add back .json()

--- .github/scripts/get_last_status.py
+++ .github/scripts/get_last_status.py
@@ -3,7 +3,7 @@ import os
 WORKFLOW_PAT = os.getenv("WORKFLOW_PAT")
 url = "https://api.github.com/repos/nationalarchives/tdr-e2e-tests/actions/workflows/ci.yml/runs"
 headers = {"Authorization": f"Bearer {WORKFLOW_PAT}"}
-resp = requests.get(url, headers=headers)
+resp = requests.get(url, headers=headers).json()
 last_status = [x for x in resp["workflow_runs"] if x["event"] == "workflow_dispatch" and x["status"] == "completed"][0]["conclusion"]
 with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
     print(f"last_status={last_status}", file=fh)

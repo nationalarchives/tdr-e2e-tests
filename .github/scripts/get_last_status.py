2ab402f726cadbb879af94659ee4e162c226f84e
Thanh Lam <thanh.lam@nationalarchives.gov.uk>
2024-12-06 11:29:10 +0000

    remove test code

--- .github/scripts/get_last_status.py
+++ .github/scripts/get_last_status.py
@@ -4,8 +4,6 @@ WORKFLOW_PAT = os.getenv("WORKFLOW_PAT")
 url = "https://api.github.com/repos/nationalarchives/tdr-e2e-tests/actions/workflows/ci.yml/runs"
 headers = {"Authorization": f"Bearer {WORKFLOW_PAT}"}
 resp = requests.get(url, headers=headers)
-print(f"Status Code: {resp.status_code}")
-print(f"Response: {resp.text}")
 last_status = [x for x in resp["workflow_runs"] if x["event"] == "workflow_dispatch" and x["status"] == "completed"][0]["conclusion"]
 with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
     print(f"last_status={last_status}", file=fh)
